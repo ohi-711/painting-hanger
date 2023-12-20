8b60c730afa4d25b180e8dd91026edf32c812c66
April <92280895+ohi-711@users.noreply.github.com>
2023-12-20 11:59:26 -0500

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -14,98 +14,98 @@ public class MainMenu
 
     public MainMenu (Console con, String menuOption)
     {
-	c = con;
-	option = menuOption;
-	draw ();
-	try // crediting Colin for his code that plays music
-	{
-	    as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
-	    AudioPlayer.player.start (as);
-	}
-	catch (Exception e)
-	{
-	}
+        c = con;
+        option = menuOption;
+        draw ();
+        try // crediting Colin for his code that plays music
+        {
+            as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
+            AudioPlayer.player.start (as);
+        }
+        catch (Exception e)
+        {
+        }
     }
 
 
     public void run ()
     {
-	animate ();
+        animate ();
     }
 
 
     public void animate ()
     {
-	while (true)
-	{
-	    
-	    draw ();
-	    pressed = c.getChar ();
-	    if (option.equals ("quit") && pressed == 'z')
-	    {
-		break;
-	    }
-	    if (pressed == '1')
-	    {
-		option = "start";
-	    }
-	    if (pressed == '2')
-	    {
-		option = "instructions";
-	    }
-	    if (pressed == '3')
-	    {
-		option = "scoreboard";
-	    }
-	    if (pressed == '4')
-	    {
-		option = "quit";
-	    }
-	}
+        while (true)
+        {
+            
+            draw ();
+            pressed = c.getChar ();
+            if (pressed == 'z')
+            {
+                break;
+            }
+            if (pressed == '1')
+            {
+                option = "start";
+            }
+            if (pressed == '2')
+            {
+                option = "instructions";
+            }
+            if (pressed == '3')
+            {
+                option = "scoreboard";
+            }
+            if (pressed == '4')
+            {
+                option = "quit";
+            }
+        }
     }
 
 
     public void draw ()
     {
-	// options
-	c.setColor (Colours.orange);
-	if (option.equals ("start"))
-	{
-	    c.setColor (Colours.darkOrange);
-	}
-	c.fillRect (130, 130, 370, 60);
+        // options
+        c.setColor (Colours.orange);
+        if (option.equals ("start"))
+        {
+            c.setColor (Colours.darkOrange);
+        }
+        c.fillRect (130, 130, 370, 60);
 
-	c.setColor (Colours.orange);
-	if (option.equals ("instructions"))
-	{
-	    c.setColor (Colours.darkOrange);
-	}
-	c.fillRect (130, 210, 370, 60);
+        c.setColor (Colours.orange);
+        if (option.equals ("instructions"))
+        {
+            c.setColor (Colours.darkOrange);
+        }
+        c.fillRect (130, 210, 370, 60);
 
-	c.setColor (Colours.orange);
-	if (option.equals ("scoreboard"))
-	{
-	    c.setColor (Colours.darkOrange);
-	}
-	c.fillRect (130, 290, 370, 60);
+        c.setColor (Colours.orange);
+        if (option.equals ("scoreboard"))
+        {
+            c.setColor (Colours.darkOrange);
+        }
+        c.fillRect (130, 290, 370, 60);
 
-	c.setColor (Colours.orange);
-	if (option.equals ("quit"))
-	{
-	    c.setColor (Colours.darkOrange);
-	}
-	c.fillRect (130, 370, 370, 60);
+        c.setColor (Colours.orange);
+        if (option.equals ("quit"))
+        {
+            c.setColor (Colours.darkOrange);
+        }
+        c.fillRect (130, 370, 370, 60);
 
-	c.setColor (Colours.black);
-	c.setFont (new Font ("Ariel", Font.BOLD, 45));
-	c.drawString ("Painting Hanger", 140, 80);
+        c.setColor (Colours.black);
+        c.setFont (new Font ("Ariel", Font.BOLD, 45));
+        c.drawString ("Painting Hanger", 140, 80);
 
-	c.setColor (Colours.white);
-	c.setFont (new Font ("Ariel", Font.BOLD, 35));
-	c.drawString ("Play", 280, 170);
-	c.drawString ("Instructions", 220, 250);
-	c.drawString ("Scoreboard", 220, 330);
-	c.drawString ("Quit", 280, 410);
+        c.setColor (Colours.white);
+        c.setFont (new Font ("Ariel", Font.BOLD, 35));
+        c.drawString ("Play", 280, 170);
+        c.drawString ("Instructions", 220, 250);
+        c.drawString ("Scoreboard", 220, 330);
+        c.drawString ("Quit", 280, 410);
 
 
     }
