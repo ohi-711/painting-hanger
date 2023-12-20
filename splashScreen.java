dfc8754b9679dd291870c3f456f82764f4402ec9
April <92280895+ohi-711@users.noreply.github.com>
2023-12-20 10:25:34 -0500

    Create splashScreen.java

---
+++ splashScreen.java
@@ -0,0 +1,69 @@
+import hsa.Console;
+import hsa.*;
+import java.io.*;
+import java.awt.*;
+import sun.audio.*;
+import java.awt.Font;
+
+public class MainMenu
+{
+    Console c;
+    String option;
+    char pressed;
+    AudioStream as; // crediting Colin for his code that plays music
+
+    public MainMenu (Console con, String menuOption)
+    {
+    	c = con;
+    	option = menuOption;
+    	draw ();
+    	try // crediting Colin for his code that plays music
+    	{
+    	    as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
+    	    AudioPlayer.player.start (as);
+    	}
+    	catch (Exception e)
+    	{
+    	}
+    }
+
+
+    public void run ()
+    {
+	    draw();
+      c.getChar();
+      // clears screen using transition
+    	c.setColor (Colours.black);
+    	for (int i = 0 ; i <= 840 ; i++)
+    	{
+    	    c.drawLine (0, i, 840, i);
+    	    try
+    	    {
+    		    Thread.sleep (3);
+    	    }
+    	    catch (Exception e)
+    	    {
+    	    }
+    	}
+    
+    	c.setColor (Colours.white);
+    	for (int i = 0 ; i <= 840 ; i++)
+    	{
+    	    c.drawLine (0, i, 840, i);
+    	    try
+    	    {
+    		    Thread.sleep (3);
+    	    }
+    	    catch (Exception e)
+    	    {
+    	    }
+    	}
+    }
+
+
+    public void draw ()
+  {
+    // add something here later for graphics
+
+    }
+}
