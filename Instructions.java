d3056ae47f7049be16ac871deceab71d69452899
April <92280895+ohi-711@users.noreply.github.com>
2023-12-20 17:53:48 -0500

    Update Instructions.java

--- Instructions.java
+++ Instructions.java
@@ -14,63 +14,36 @@ public class Instructions
 
     public Instructions (Console con)
     {
-	c = con;
-	draw ();
-	try // crediting Colin for his code that plays music
-	{
-	    as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
-	    AudioPlayer.player.start (as);
-	}
-	catch (Exception e)
-	{
-	}
+        c = con;
+        draw ();
+        try // crediting Colin for his code that plays music
+        {
+            as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
+            AudioPlayer.player.start (as);
+        }
+        catch (Exception e)
+        {
+        }
     }
 
 
     public void run ()
     {
-	draw ();
-	c.getChar ();
-	// clears screen using transition
-	c.setColor (Colours.black);
-	for (int i = 0 ; i <= 840 ; i++)
-	{
-	    c.drawLine (0, i, 840, i);
-	    try
-	    {
-		Thread.sleep (3);
-	    }
-	    catch (Exception e)
-	    {
-	    }
-	}
-
-	c.setColor (Colours.white);
-	for (int i = 0 ; i <= 840 ; i++)
-	{
-	    c.drawLine (0, i, 840, i);
-	    try
-	    {
-		Thread.sleep (3);
-	    }
-	    catch (Exception e)
-	    {
-	    }
-	}
+        draw ();
     }
 
 
-    public void draw ()
-    {
-	c.setColor (Colours.black);
-	c.setFont (new Font ("Ariel", Font.BOLD, 45));
-	c.drawString ("Painting Hanger", 140, 80);
-	c.drawString ("Go through the basement", 140, 80);
-	c.drawString ("to get the paintings,", 140, 90);
-	c.drawString ("and then put the", 140, 100);
-	c.drawString ("paintings up on the", 140, 110);
-	c.drawString ("living room wall!", 140, 120);
-	// add more here later for graphics
+public void draw ()
+{
+    c.setColor (Colours.black);
+    c.setFont (new Font ("Ariel", Font.BOLD, 45));
+    c.drawString ("Painting Hanger", 140, 80);
+    c.drawString ("Go through the basement", 140, 80);
+    c.drawString ("to get the paintings,", 140, 90);
+    c.drawString ("and then put the", 140, 100);
+    c.drawString ("paintings up on the", 140, 110);
+    c.drawString ("living room wall!", 140, 120);
+    // add more here later for graphics
 
-    }
+}
 }
