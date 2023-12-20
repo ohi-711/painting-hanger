2e8958db13b4c318ae8f3ac37607f991a32a4d1a
April <92280895+ohi-711@users.noreply.github.com>
2023-12-20 11:59:42 -0500

    Update PaintingHanger.java

--- PaintingHanger.java
+++ PaintingHanger.java
@@ -8,23 +8,32 @@ public class PaintingHanger {
     
 
     public PaintingHanger(){
-	c = new Console("Painting Hanger");
+        c = new Console("Painting Hanger");
     }
 
     public void splashScreen(){
-	SplashScreen s = new SplashScreen(c);
-	s.run();	
+        SplashScreen s = new SplashScreen(c);
+        s.run();        
     }
-	
+        
     public void menu(){
-	MainMenu m = new MainMenu(c, menuOption);
-	m.run();
+        MainMenu m = new MainMenu(c, menuOption);
+        m.run();
+    }
+    
+    public void instructions(){
+        Instructions i = new Instructions(c);
+        i.run();
     }
 
     public static void main(String[] args) {
-	PaintingHanger p = new PaintingHanger();
-	p.splashScreen();
-	p.menu();
+        PaintingHanger p = new PaintingHanger();
+        p.splashScreen();
+        p.menu();
+        if (p.menuOption.equals("instructions"))
+        {
+            p.instructions();
+        }
 
     }
 }
