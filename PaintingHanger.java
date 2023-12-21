d9f425f596942bed838c83a9e8518a92f512dd4c
April <92280895+ohi-711@users.noreply.github.com>
2023-12-20 19:33:49 -0500

    Update PaintingHanger.java

--- PaintingHanger.java
+++ PaintingHanger.java
@@ -2,38 +2,55 @@ import hsa.Console;
 import java.lang.*;
 import java.awt.*;
 
-public class PaintingHanger {
+public class PaintingHanger
+{
     Console c;
     String menuOption = "play";
-    
 
-    public PaintingHanger(){
-        c = new Console("Painting Hanger");
+
+    public PaintingHanger ()
+    {
+        c = new Console ("Painting Hanger");
     }
 
-    public void splashScreen(){
-        SplashScreen s = new SplashScreen(c);
-        s.run();        
+
+    public void splashScreen ()
+    {
+        SplashScreen s = new SplashScreen (c);
+        s.run ();
     }
-        
-    public void menu(){
-        MainMenu m = new MainMenu(c, menuOption);
-        m.run();
+
+
+    public void menu ()
+    {
+        MainMenu m = new MainMenu (c, menuOption);
+        m.run ();
         menuOption = m.option;
     }
-    
-    public void instructions(){
-        Instructions i = new Instructions(c);
-        i.run();
+
+
+    public void instructions ()
+    {
+        Instructions i = new Instructions (c);
+        i.run ();
     }
 
-    public static void main(String[] args) {
-        PaintingHanger p = new PaintingHanger();
-        p.splashScreen();
-        p.menu();
-        if (p.menuOption.equals("instructions"))
+
+    public static void main (String[] args)
+    {
+        PaintingHanger p = new PaintingHanger ();
+        p.splashScreen ();
+        while (true)
         {
-            p.instructions();
+            p.menu ();
+            if (p.menuOption.equals ("instructions"))
+            {
+                p.instructions ();
+            }
+            if (p.menuOption.equals ("quit"))
+            {
+                break;
+            }
         }
 
     }
