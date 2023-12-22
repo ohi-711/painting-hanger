ffe920bf1d3d3b0ec9617c7ea02d6cef2eba3686
April <92280895+ohi-711@users.noreply.github.com>
2023-12-22 11:13:04 -0500

    Update Goodbye.java

--- Goodbye.java
+++ Goodbye.java
@@ -14,56 +14,56 @@ public class Goodbye
 
     public Goodbye (Console con)
     {
-	c = con;
-	try // crediting Colin for his code that plays music
-	{
-	    as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
-	    AudioPlayer.player.start (as);
-	}
-	catch (Exception e)
-	{
-	}
+        c = con;
+        try // crediting Colin for his code that plays music
+        {
+            as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
+            AudioPlayer.player.start (as);
+        }
+        catch (Exception e)
+        {
+        }
     }
 
 
     public void run ()
     {
-	// clears screen using transition
-	c.setColor (Colours.black);
-	for (int i = 0 ; i <= 840 ; i++)
-	{
-	    c.drawLine (0, i, 840, i);
-	    try
-	    {
-		Thread.sleep (3);
-	    }
-	    catch (Exception e)
-	    {
-	    }
-	}
+        // clears screen using transition
+        c.setColor (Colours.black);
+        for (int i = 0 ; i <= 840 ; i++)
+        {
+            c.drawLine (0, i, 840, i);
+            try
+            {
+                Thread.sleep (3);
+            }
+            catch (Exception e)
+            {
+            }
+        }
 
-	c.setColor (Colours.white);
-	for (int i = 0 ; i <= 840 ; i++)
-	{
-	    c.drawLine (0, i, 840, i);
-	    try
-	    {
-		Thread.sleep (3);
-	    }
-	    catch (Exception e)
-	    {
-	    }
-	}
-	draw ();
+        c.setColor (Colours.white);
+        for (int i = 0 ; i <= 840 ; i++)
+        {
+            c.drawLine (0, i, 840, i);
+            try
+            {
+                Thread.sleep (3);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+        draw ();
     }
 
 
     public void draw ()
     {
-	c.setColor (Colours.black);
-	c.setFont (new Font ("Ariel", Font.BOLD, 45));
-	c.drawString ("Thank you for playing!", 100, 80);
-	// add more here later for graphics
+        c.setColor (Colours.black);
+        c.setFont (new Font ("Ariel", Font.BOLD, 45));
+        c.drawString ("Thank you for playing!", 90, 80);
+        // add more here later for graphics
 
     }
 }
