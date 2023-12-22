ff9a7c78508d00901e1aed4299a7ac9486184e93
April <92280895+ohi-711@users.noreply.github.com>
2023-12-22 10:55:41 -0500

    Update Colours.java

--- Colours.java
+++ Colours.java
@@ -4,5 +4,6 @@ public class Colours {
     public static Color darkOrange = new Color(214, 159, 75);
     public static Color black = new Color(0, 0, 0);
     public static Color white = new Color(255, 255, 255);
+    public static Color lightBlue = new Color(208, 228, 255);
 
 }
