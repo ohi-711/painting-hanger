6de9ad82ae06711552778461276b64724d1f7f8c
April <92280895+ohi-711@users.noreply.github.com>
2023-12-22 11:41:18 -0500

    Add files via upload

---
+++ drawCharacter.java
@@ -0,0 +1,27 @@
+import hsa.Console;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import java.util.*;
+
+public class drawCharacter
+{
+    Console c;
+    String option;
+    char pressed;
+
+    public drawCharacter (Console con, int locationX, int locationY)
+    {
+	c = con;
+	draw (locationX, locationY);
+    }
+
+
+
+    public void draw (int locationX, int locationY)
+    {
+	
+    }
+}
