a06a6025964052d37e5df90f8197766467d6c0c8
April <92280895+ohi-711@users.noreply.github.com>
2023-12-22 10:46:10 -0500

    Add files via upload

---
+++ Scoreboard.java
@@ -0,0 +1,73 @@
+import hsa.Console;
+import hsa.*;
+import java.io.*;
+import java.awt.*;
+import sun.audio.*;
+import java.awt.Font;
+
+public class Scoreboard
+{
+    Console c;
+    String option;
+    char pressed;
+    AudioStream as; // crediting Colin for his code that plays music
+
+    public Scoreboard (Console con)
+    {
+	c = con;
+	draw ();
+	try // crediting Colin for his code that plays music
+	{
+	    as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
+	    AudioPlayer.player.start (as);
+	}
+	catch (Exception e)
+	{
+	}
+    }
+
+
+    public void run ()
+    {
+	draw ();
+    }
+
+
+    public void draw ()
+    {
+	char pressed = c.getChar ();
+	c.setColor (Colours.white);
+	c.fillRect (0, 0, 640, 500);
+	c.setColor (Colours.black);
+	c.setFont (new Font ("Ariel", Font.BOLD, 45));
+	c.drawString ("High Scores", 140, 80);
+	
+	c.setFont (new Font ("Ariel", Font.PLAIN, 35));
+	try
+	{
+	    BufferedReader in;
+	    String line;
+	    int count = 0;
+	    boolean foundName = false;
+	    in = new BufferedReader (new FileReader ("scoreboard.txt"));
+	    while (true)
+	    {
+		count++;
+		// if the store name is found:
+		line = in.readLine ();
+		if (count > 8)
+		    break;
+		if (line == null)
+		    break;
+		c.drawString (line, 140, 120+count*35);
+	    }
+	    in.close ();
+	}
+	catch (IOException e)
+	{
+	}
+
+	c.setFont (new Font ("Ariel", Font.BOLD, 25));
+	c.drawString ("Press any key to return", 140, 450);
+    }
+}
