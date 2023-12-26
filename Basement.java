82ff71785cf85a8463e4688fb1ed68f0021c7cb9
April <92280895+ohi-711@users.noreply.github.com>
2023-12-25 23:14:36 -0500

    Add files via upload

---
+++ Basement.java
@@ -0,0 +1,79 @@
+import hsa.Console;
+import hsa.*;
+import java.io.*;
+import java.awt.*;
+import sun.audio.*;
+import java.awt.Font;
+
+public class Basement
+{
+    Console c;
+    String option;
+    int locationX;
+    int locationY;
+    char pressed;
+    String direction = "front";
+    AudioStream as; // crediting Colin for his code that plays music
+
+    public Basement (Console con, int locationX, int locationY)
+    {
+	c = con;
+	locationX = locationX;
+	locationY = locationY;
+	walk ();
+	try // crediting Colin for his code that plays music
+	{
+	    as = new AudioStream (new FileInputStream ("88. Lost Library.wav"));
+	    AudioPlayer.player.start (as);
+	}
+	catch (Exception e)
+	{
+	}
+    }
+
+
+    public void run ()
+    {
+	drawCharacter ();
+    }
+
+
+    public void walk ()
+    {
+	char pressed = c.getChar ();
+
+
+	if (pressed == 'w' && locationY - 20 > 0)
+	{
+	    locationY -= 20;
+	    direction = "front";
+	}
+	else if (pressed == 'd' && locationX + 20 + 50 < 640) // 50 is character width
+	{
+	    locationX += 20;
+	    direction = "right";
+	}
+	else if (pressed == 's' && locationY + 20 + 50 < 500) // 50 is character height
+	{
+	    locationY += 20;
+	    direction = "back";
+	}
+	else if (pressed == 'a' && locationX - 20 > 0)
+	{
+	    locationX -= 20;
+	    direction = "left";
+	}
+
+    }
+
+
+    public void drawCharacter ()
+    {
+	drawCharacter d = new drawCharacter (c, locationX, locationY, direction);
+	while (true)
+	{
+	    walk ();
+	    d.draw (c, locationX, locationY, direction);
+	}
+    }
+}
--- drawCharacter.java
+++ drawCharacter.java
@@ -11,23 +11,46 @@ public class drawCharacter
     Console c;
     String option;
     char pressed;
+    BufferedImage character;
 
-    public drawCharacter (Console con, int locationX, int locationY)
+    public drawCharacter (Console con, int locationX, int locationY, String direction)
     {
 	c = con;
-	draw (locationX, locationY);
+	draw (c, locationX, locationY, direction);
     }
 
 
-    public void draw (int locationX, int locationY)
+    public void draw (Console con, int locationX, int locationY, String direction)
     {
+	c = con;
+	// imports image
 	try
-	{ // imports image
-	   BufferedImage menuBackground = ImageIO.read (new File ("character.png"));
-	   c.drawImage (menuBackground, locationX, locationY, null);
+	{
+	    if (direction.equals ("front"))
+	    {
+		character = ImageIO.read (new File ("front_character.jpg"));
+	    }
+	    else if (direction.equals ("back"))
+	    {
+		character = ImageIO.read (new File ("back_character.jpg"));
+	    }
+	    else if (direction.equals ("left"))
+	    {
+		character = ImageIO.read (new File ("left_character.jpg"));
+	    }
+	    else if (direction.equals ("right"))
+	    {
+		character = ImageIO.read (new File ("right_character.jpg"));
+	    }
 	}
 	catch (IOException e)
 	{
 	}
+
+
+	c.setColor (Colours.white);
+	c.fillRect (0, 0, 640, 500);
+	c.drawImage (character, locationX, locationY, null);
+
     }
 }
