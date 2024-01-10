9b6ca1c71a040eb3eace9b444a5d593db13e13ad
April <92280895+ohi-711@users.noreply.github.com>
2024-01-10 14:46:13 -0500

    Create PaintingInstructions.java

---
+++ PaintingInstructions.java
@@ -0,0 +1,46 @@
+import hsa.Console;
+import hsa.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.Font;
+
+public class PaintingInstructions
+{
+    Console c;
+    String option;
+    char pressed;
+
+    public PaintingInstructions (Console con)
+    {
+        c = con;
+        draw ();
+    }
+
+
+    public void run ()
+    {
+        draw ();
+    }
+
+
+    public void draw ()
+    {
+        char pressed = c.getChar ();
+        c.setColor (Colours.white);
+        c.fillRect (0, 0, 640, 500);
+        c.setColor (Colours.black);
+        c.setFont (new Font ("Ariel", Font.BOLD, 45));
+        c.drawString ("Painting Hanger", 140, 80);
+
+        c.setFont (new Font ("Ariel", Font.PLAIN, 35));
+        c.drawString ("Go through the", 140, 140);
+        c.drawString ("basement to get", 140, 180);
+        c.drawString ("the paintings,", 140, 220);
+        c.drawString ("and then put the", 140, 260);
+        c.drawString ("paintings up on the", 140, 300);
+        c.drawString ("living room wall!", 140, 340);
+
+        c.setFont (new Font ("Ariel", Font.BOLD, 25));
+        c.drawString ("Press any key to return", 140, 450);
+    }
+}
