0bc7a0e90de99238fc10fff023239131bee15f20
April <92280895+ohi-711@users.noreply.github.com>
2024-01-11 09:59:30 -0500

    Update PaintingInstructions.java

--- PaintingInstructions.java
+++ PaintingInstructions.java
@@ -25,19 +25,40 @@ public class PaintingInstructions
         Alice a = new Alice (c);
         a.display (200, 250);
         
-        draw ();
+        dialogue ();
     }
 
 
-    public void draw ()
+    public void dialogue ()
     {
+        LivingRoomNone l2 = new LivingRoomNone (c);
+        Alice a2 = new Alice (c);
+        
         c.getChar ();
         c.setColor (Colours.black);
         c.setFont (new Font ("Ariel", Font.PLAIN, 30));
         c.drawString ("instructions here", 140, 140);
-
-
         c.setFont (new Font ("Ariel", Font.BOLD, 25));
         c.drawString ("Press any key to continue", 140, 450);
+        c.getChar ();
+        
+        l2.display();
+        a2.display (200, 250);
+        
+        c.setFont (new Font ("Ariel", Font.PLAIN, 30));
+        c.c.drawString ("more instructions here", 140, 140);
+        c.setFont (new Font ("Ariel", Font.BOLD, 25));
+        c.drawString ("Press any key to continue", 140, 450);
+        c.getChar ();
+
+        l2.display();
+        a2.display (200, 250);
+
+        c.setFont (new Font ("Ariel", Font.PLAIN, 30));
+        c.c.drawString ("thank you for your help!!!!", 140, 140);
+        c.setFont (new Font ("Ariel", Font.BOLD, 25));
+        c.drawString ("Press any key to continue", 140, 450);
+        
+        c.getChar ();
     }
 }
