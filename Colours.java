6dc9200bc84d47303167543274dca59825c02cac
U-starbucks <124642126+U-starbucks@users.noreply.github.com>
2024-01-12 11:15:37 -0500

    Update Colours.java

--- Colours.java
+++ Colours.java
@@ -1,9 +1,68 @@
 import java.awt.*;
 public class Colours {
+
+    //Main Menu 
     public static Color orange = new Color(255, 189, 89);
     public static Color darkOrange = new Color(214, 159, 75);
     public static Color black = new Color(0, 0, 0);
     public static Color white = new Color(255, 255, 255);
     public static Color lightBlue = new Color(208, 228, 255);
     public static Color red = new Color(247, 67, 67);
+    
+    //Living Room
+    public static Color wall = new Color (204, 219, 253);
+    public static Color grout = new Color (171, 196, 255);
+    public static Color tile = new Color (182, 204, 254);
+    public static Color lining = new Color (226, 234, 252);
+    public static Color cOpening = new Color (171, 196, 255);
+    public static Color cabinetLeg = new Color (115, 82, 56);
+    public static Color fPot = new Color (255, 196, 214);
+    public static Color lsb = new Color (255, 135, 171);
+    public static Color lst = new Color (255, 166, 193);
+    public static Color lb = new Color (139, 140, 137);
+    public static Color cSide = new Color (142, 148, 242);
+    public static Color cMiddle = new Color (173, 167, 255);
+    public static Color cTop = new Color (129, 135, 220);
+    public static Color gOne = new Color (76, 175, 80);
+    public static Color gTwo = new Color (139, 195, 74);
+    public static Color y = new Color (205, 220, 57);
+
+    //Paintings
+    public static Color frame = new Color (115, 82, 56);
+    
+    //Painting One
+    public static Color mountain = new Color (40, 53, 147);
+    public static Color mountainOutline = new Color (48, 63, 159);
+    public static Color sky = new Color (69, 39, 160);
+    public static Color moon = new Color (230, 238, 156);
+    
+    //Painting Two
+    public static Color twoblueDark = new Color (63, 81, 181);
+    public static Color twoblueLight = new Color (33, 150, 243);
+    public static Color twobackground = new Color (255, 255, 255);
+    public static Color twogrey = new Color (96, 125, 139);
+    public static Color twopurple = new Color (103, 58, 183);
+    public static Color twoyellow = new Color (255, 235, 59);
+    
+    //Painting Three
+    public static Color threebackground = new Color (76, 175, 80);
+    public static Color threeblue = new Color (0, 145, 234);
+    public static Color threepurple = new Color (103, 58, 183);
+    public static Color threeorange = new Color (255, 167, 38);
+    public static Color threeyellow = new Color (255, 235, 59);
+    public static Color threepink = new Color (239, 154, 154);
+    
+    //Painting Four
+    public static Color dogMain = new Color (171, 196, 255);
+    public static Color dogSub = new Color (63, 81, 181);
+    public static Color fourbackground = new Color (204, 219, 253);
+    public static Color eyes = new Color (0, 0, 0);
+    public static Color tongue = new Color (233, 30, 99);
+    
+    //Painting Five
+    public static Color fivegrayDark = new Color (55, 71, 79);
+    public static Color fivegrayLight = new Color (69, 90, 100);
+    public static Color fiveorange = new Color (255, 110, 64);
+    public static Color fiveorangeLight = new Color (255, 171, 0);
+    public static Color fiveyellow = new Color (255, 202, 40);
 }
