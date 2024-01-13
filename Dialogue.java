4022ed9db59978a7bbb33e9c49f7a7e135209299
April <92280895+ohi-711@users.noreply.github.com>
2024-01-13 11:54:05 -0500

    commenting

--- Dialogue.java
+++ Dialogue.java
@@ -1,3 +1,12 @@
+/*
+ * Name: April Sun, Daisy Li, Anne You
+ * Date: January 13th 2023
+ * Teacher: Ms. Krasteva
+ * Description: This program displays the dialogue line by line by looping through an array of strings. 
+ */
+
+// Daisy (with additions by April)
+// imports
 import hsa.Console;
 import hsa.*;
 import java.io.*;
@@ -6,19 +15,18 @@ import java.awt.Font;
 
 public class Dialogue
 {
+    // April
     Console c;
-    String option;
-    char pressed;
-    String[] dialogue = {};
-
+    String[] dialogue = {}; // string array of the dialogue
+    
     public Dialogue (Console con, String[] d)
     {
         c = con;
         dialogue = d;
         LivingRoom l = new LivingRoom (c);
-        l.display ();
+        l.display (); // displays the living room as the background
         Alice a = new Alice (c);
-        a.display (200, 250);
+        a.display (200, 250); // displays the character talking to the player
     }
 
 
@@ -27,8 +35,8 @@ public class Dialogue
         dialogue ();
     }
 
-
-    public void dialogue ()
+    // Daisy
+    public void dialogue () // displays the dialogue
     {
         LivingRoomNone l2 = new LivingRoomNone (c);
         Alice a2 = new Alice (c);
@@ -39,7 +47,7 @@ public class Dialogue
         c.setFont (new Font ("Ariel", Font.BOLD, 25));
         c.drawString ("Press any key to continue", 140, 450);
         c.getChar();
-        for (int i = 0 ; i < dialogue.length; i++)
+        for (int i = 0 ; i < dialogue.length; i++) // loops through the string array of the dialogue
         {
             l2.display ();
             a2.display (200, 250);
