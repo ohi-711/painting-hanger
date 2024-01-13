b93b857c254e8f8b47c2215aeb4dfba16602dc3b
April <92280895+ohi-711@users.noreply.github.com>
2024-01-13 11:34:57 -0500

    comments go brr

--- MainMenu.java
+++ MainMenu.java
@@ -1,3 +1,12 @@
+/*
+ * Name: April Sun, Daisy Li, Anne You
+ * Date: January 13th 2023
+ * Teacher: Ms. Krasteva
+ * Description: This is the main menu of the game. This screen appears after the splash screen and the user can choose between playing the game, reading the instructions, checking the leaderboard, or exiting the game.
+ */
+
+// April
+// imports
 import hsa.Console;
 import hsa.*;
 import java.io.*;
@@ -7,8 +16,8 @@ import java.awt.Font;
 public class MainMenu
 {
     Console c;
-    String option;
-    char pressed;
+    String option; // option the user selected
+    char pressed; // current key pressed by the user
 
     public MainMenu (Console con, String menuOption)
     {
@@ -25,7 +34,7 @@ public class MainMenu
             
             draw ();
             pressed = c.getChar ();
-            
+            // the player uses 1, 2, 3, 4 to select their option
             if (pressed == '1')
             {
                 option = "start";
@@ -42,7 +51,7 @@ public class MainMenu
             {
                 option = "quit";
             }
-            if (pressed == 'z')
+            if (pressed == 'z') // the user presses z to confirm their menu selection
             {
                 break;
             }
@@ -51,13 +60,13 @@ public class MainMenu
 
     public void draw ()
     {
-        // options
+        // draws the boxes for the options
         c.setColor (Colours.white);
         c.fillRect (0, 0, 640, 500);
         c.setColor (Colours.orange);
         if (option.equals ("start"))
         {
-            c.setColor (Colours.darkOrange);
+            c.setColor (Colours.darkOrange); // the current option the user is selecting is darkened to indicate their selection
         }
         c.fillRect (130, 130, 370, 60);
 
@@ -84,11 +93,11 @@ public class MainMenu
 
         c.setColor (Colours.black);
         c.setFont (new Font ("Ariel", Font.BOLD, 45));
-        c.drawString ("Painting Hanger", 140, 80);
+        c.drawString ("Painting Hanger", 140, 80); // writes the title at the top of the menu
 
         c.setColor (Colours.white);
         c.setFont (new Font ("Ariel", Font.BOLD, 35));
-        c.drawString ("Play", 280, 170);
+        c.drawString ("Play", 280, 170); // text inside of the menu box options
         c.drawString ("Instructions", 220, 250);
         c.drawString ("Scoreboard", 220, 330);
         c.drawString ("Quit", 280, 410);
