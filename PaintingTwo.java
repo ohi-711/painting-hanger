e4a1c2b774e7c6fe34269b4f3a4fa43379af1a3a
U-starbucks <124642126+U-starbucks@users.noreply.github.com>
2024-01-13 21:35:34 -0500

    Update PaintingTwo.java

--- PaintingTwo.java
+++ PaintingTwo.java
@@ -2,73 +2,106 @@ import java.awt.*;
 import hsa.Console;
 
 /**
-* This program draws the second painting of Anne, Daisy and April's ISP for ICS3UP 
+* This program draws the third painting of Anne, Daisy and April's ISP for ICS3UP 
 * @author Anne You
 * @teacher Ms. Krasteva
 * @assignment ISP
 * January 12th, 2024
 */
 
-public class PaintingTwo
+public class PaintingThree
 {
     Console c;
 
-    public PaintingTwo (Console con)
+    public PaintingThree (Console con)
     {
         c = con;
     }
 
-    //In display(), Painting number two, an abstract painting, is drawn
+    
+    //In display(), Painting number three, an abstract painting, is drawn
     public void display (int x, int y)
     {
         //Frame
         c.setColor (Colours.frame);
-        c.fillRect (0 + x, 0 + y, 130, 10);
-        c.fillRect (0 + x, 120 + y, 130, 10);
-        c.fillRect (0 + x, 0 + y, 10, 130);
-        c.fillRect (120 + x, 0 + y, 10, 130);
-        //White Background
-        c.setColor (Colours.twobackground);
-        c.fillRect (10 + x, 10 + y, 110, 110);
-        //Grey
-        c.setColor (Colours.twogrey);
-        c.fillRect (10 + x, 30 + y, 10, 10);
-        c.fillRect (20 + x, 20 + y, 20, 10);
-        c.fillRect (40 + x, 30 + y, 10, 10);
-        c.fillRect (50 + x, 40 + y, 10, 20);
-        c.fillRect (10 + x, 60 + y, 10, 10);
-        c.fillRect (20 + x, 70 + y, 20, 10);
-        c.fillRect (40 + x, 60 + y, 10, 10);
-        c.fillRect (80 + x, 100 + y, 10, 20);
-        c.fillRect (100 + x, 80 + y, 20, 10);
-        c.fillRect (90 + x, 90 + y, 10, 10);
-        //Yellow
-        c.setColor (Colours.twoyellow);
-        c.fillRect (30 + x, 40 + y, 10, 10);
-        c.fillRect (30 + x, 50 + y, 20, 10);
-        //Dark Blue
-        c.setColor (Colours.twoblueDark);
-        c.fillRect (30 + x, 90 + y, 10, 10);
-        c.fillRect (40 + x, 80 + y, 20, 10);
-        c.fillRect (50 + x, 60 + y, 10, 20);
-        c.fillRect (60 + x, 50 + y, 20, 20);
-        c.fillRect (70 + x, 30 + y, 10, 20);
-        c.fillRect (80 + x, 40 + y, 10, 20);
-        c.fillRect (90 + x, 60 + y, 10, 10);
-        c.fillRect (100 + x, 40 + y, 10, 10);
-        c.fillRect (70 + x, 80 + y, 10, 20);
+        c.fillRect (0 + x, 0 + y, 250, 10);
+        c.fillRect (0 + x, 170 + y, 250, 10);
+        c.fillRect (0 + x, 0 + y, 10, 170);
+        c.fillRect (240 + x, 0 + y, 10, 170);
+        //Green Background
+        c.setColor (Colours.threebackground);
+        c.fillRect (10 + x, 10 + y, 230, 160);
+        //Blue
+        c.setColor (Colours.threeblue);
+        c.fillRect (10 + x, 50 + y, 10, 10);
+        c.fillRect (10 + x, 40 + y, 30, 10);
+        c.fillRect (30 + x, 30 + y, 20, 10);
+        c.fillRect (50 + x, 20 + y, 40, 10);
+        c.fillRect (80 + x, 10 + y, 30, 10);
+        c.fillRect (10 + x, 80 + y, 20, 10);
+        c.fillRect (20 + x, 70 + y, 30, 10);
+        c.fillRect (50 + x, 60 + y, 20, 10);
+        c.fillRect (60 + x, 50 + y, 20, 10);
+        c.fillRect (80 + x, 40 + y, 20, 10);
+        c.fillRect (90 + x, 30 + y, 20, 10);
+        c.fillRect (110 + x, 20 + y, 30, 10);
+        c.fillRect (130 + x, 10 + y, 20, 10);
+        c.fillRect (10 + x, 110 + y, 40, 10);
+        c.fillRect (50 + x, 100 + y, 30, 10);
         c.fillRect (80 + x, 90 + y, 10, 10);
-        //Light Blue
-        c.setColor (Colours.twoblueLight);
-        c.fillRect (30 + x, 80 + y, 10, 10);
-        c.fillRect (40 + x, 70 + y, 10, 10);
-        c.fillRect (70 + x, 20 + y, 10, 10);
-        c.fillRect (80 + x, 30 + y, 10, 10);
-        c.fillRect (90 + x, 50 + y, 20, 10);
-        c.fillRect (90 + x, 100 + y, 10, 10);
+        c.fillRect (90 + x, 80 + y, 10, 10);
+        c.fillRect (110 + x, 70 + y, 30, 10);
+        c.fillRect (140 + x, 60 + y, 10, 10);
+        c.fillRect (150 + x, 50 + y, 30, 10);
+        c.fillRect (170 + x, 40 + y, 20, 10);
+        c.fillRect (180 + x, 30 + y, 20, 10);
+        c.fillRect (190 + x, 20 + y, 10, 10);
         //Purple
-        c.setColor (Colours.twopurple);
-        c.fillRect (100 + x, 100 + y, 20, 10);
-        c.fillRect (110 + x, 110 + y, 10, 10);
+        c.setColor (Colours.threepurple);
+        c.fillRect (50 + x, 30 + y, 20, 10);
+        c.fillRect (40 + x, 40 + y, 40, 10);
+        c.fillRect (40 + x, 50 + y, 20, 10);
+        //Orange
+        c.setColor (Colours.threeorange);
+        c.fillRect (30 + x, 140 + y, 10, 10);
+        c.fillRect (40 + x, 130 + y, 10, 10);
+        c.fillRect (120 + x, 130 + y, 30, 10);
+        c.fillRect (50 + x, 120 + y, 70, 10);
+        c.fillRect (150 + x, 120 + y, 40, 10);
+        c.fillRect (100 + x, 110 + y, 40, 10);
+        c.fillRect (180 + x, 110 + y, 20, 10);
+        c.fillRect (90 + x, 90 + y, 10, 20);
+        c.fillRect (100 + x, 70 + y, 10, 20);
+        c.fillRect (110 + x, 50 + y, 10, 20);
+        c.fillRect (120 + x, 50 + y, 30, 10);
+        c.fillRect (150 + x, 60 + y, 10, 20);
+        c.fillRect (160 + x, 70 + y, 10, 30);
+        c.fillRect (140 + x, 100 + y, 20, 10);
+        c.fillRect (190 + x, 100 + y, 10, 10);
+        c.fillRect (200 + x, 90 + y, 10, 20);
+        c.fillRect (210 + x, 40 + y, 10, 50);
+        c.fillRect (200 + x, 10 + y, 10, 40);
+        c.fillRect (210 + x, 10 + y, 10, 10);
+        //Pink
+        c.setColor (Colours.threepink);
+        c.fillRect (50 + x, 150 + y, 10, 10);
+        c.fillRect (60 + x, 140 + y, 10, 20);
+        c.fillRect (80 + x, 130 + y, 10, 10);
+        c.fillRect (90 + x, 100 + y, 10, 10);
+        c.fillRect (120 + x, 120 + y, 20, 10);
+        c.fillRect (110 + x, 130 + y, 10, 10);
+        c.fillRect (90 + x, 140 + y, 30, 10);
+        c.fillRect (90 + x, 150 + y, 130, 10);
+        //Yellow
+        c.setColor (Colours.threeyellow);
+        c.fillRect (230 + x, 30 + y, 10, 120);
+        c.fillRect (220 + x, 40 + y, 10, 110);
+        c.fillRect (210 + x, 90 + y, 10, 50);
+        c.fillRect (200 + x, 110 + y, 10, 20);
+        c.fillRect (200 + x, 50 + y, 10, 40);
+        c.fillRect (190 + x, 60 + y, 10, 40);
+        c.fillRect (180 + x, 70 + y, 10, 40);
+        c.fillRect (170 + x, 80 + y, 10, 30);
+        c.fillRect (150 + x, 90 + y, 10, 10);
     }
 }
