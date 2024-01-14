1db79f361b08d15fbc600def634fa317fbf13f94
U-starbucks <124642126+U-starbucks@users.noreply.github.com>
2024-01-13 21:19:16 -0500

    Update Alice.java

--- Alice.java
+++ Alice.java
@@ -1,6 +1,14 @@
 import java.awt.*;
 import hsa.Console;
 
+/**
+* This program draws Alice, a character in Anne, Daisy and April's ISP for ICS3UP
+* @author Anne You
+* @teacher Ms. Krasteva
+* @assignment ISP
+* January 12th, 2024
+*/
+
 public class Alice
 {
     Console c;
@@ -10,16 +18,22 @@ public class Alice
         c = con;
     }
 
+
+    //In display(), the character, Alice, is drawn
     public void display (int x, int y)
     {
+        //Face
         c.setColor (Colours.skin);
         c.fillRect (10 + x, 20 + y, 70, 50);
+        //Shirt
         c.setColor (Colours.shirt);
         c.fillRect (20 + x, 70 + y, 60, 80);
+        //Pants
         c.setColor (Colours.pants);
         c.fillRect (10 + x, 150 + y, 30, 100);
         c.fillRect (50 + x, 150 + y, 30, 100);
         c.fillRect (40 + x, 150 + y, 10, 30);
+        //Hair + Eyes
         c.setColor (Colours.brown);
         c.fillRect (20 + x, 30 + y, 10, 20);
         c.fillRect (60 + x, 30 + y, 10, 20);
