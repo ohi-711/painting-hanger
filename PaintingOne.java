45ed4b6b3764d71126209d402a9bfbbdb0387eb0
U-starbucks <124642126+U-starbucks@users.noreply.github.com>
2024-01-13 21:27:54 -0500

    Update PaintingOne.java

--- PaintingOne.java
+++ PaintingOne.java
@@ -1,6 +1,14 @@
 import java.awt.*;
 import hsa.Console;
 
+/**
+* This program draws the first painting of Anne, Daisy and April's ISP for ICS3UP
+* @author Anne You
+* @teacher Ms. Krasteva
+* @assignment ISP
+* January 12th, 2024
+*/
+
 public class PaintingOne
 {
     Console c;
@@ -10,15 +18,20 @@ public class PaintingOne
         c = con;
     }
 
+
+    //In display(), Painting number one of a moon is drawn
     public void display (int x, int y)
     {
+        //Frame
         c.setColor (Colours.frame);
         c.fillRect (x, y, 220, 10);
         c.fillRect (x, y + 150, 220, 10);
         c.fillRect (x, y, 10, 150);
         c.fillRect (x + 220, y, 10, 160);
+        //Sky(Background Colour)
         c.setColor (Colours.sky);
         c.fillRect (x + 10, y + 10, 210, 130);
+        //Mountains/Ground
         c.setColor (Colours.mountain);
         c.fillRect (10 + x, 140 + y, 140, 10);
         c.fillRect (170 + x, 140 + y, 50, 10);
@@ -48,6 +61,7 @@ public class PaintingOne
         c.fillRect (170 + x, 130 + y, 10, 10);
         c.fillRect (180 + x, 120 + y, 20, 10);
         c.fillRect (200 + x, 110 + y, 20, 10);
+        //Moon
         c.setColor (Colours.moon);
         c.fillRect (80 + x, 20 + y, 50, 10);
         c.fillRect (110 + x, 30 + y, 30, 10);
@@ -58,4 +72,3 @@ public class PaintingOne
         c.fillRect (100 + x, 100 + y, 30, 10);
     }
 }
-
