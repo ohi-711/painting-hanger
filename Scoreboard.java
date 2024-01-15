186107af37c90ac278802e4db09a1bc4c406e4d6
April <92280895+ohi-711@users.noreply.github.com>
2024-01-14 21:24:03 -0500

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,3 +1,12 @@
+/*
+ * Name: April Sun, Daisy Li, Anne You
+ * Date: January 13th 2023
+ * Teacher: Ms. Krasteva
+ * Description: This is the scoreboard screen of the game. This screen appears after the menu if the user chose to look at the scoreboard
+ */
+
+// April
+// imports
 import hsa.Console;
 import hsa.*;
 import java.io.*;
@@ -8,22 +17,12 @@ import java.awt.Font;
 public class Scoreboard
 {
     Console c;
-    String option;
-    char pressed;
     AudioStream as; // crediting Colin for his code that plays music
 
     public Scoreboard (Console con)
     {
         c = con;
         draw ();
-        try // crediting Colin for his code that plays music
-        {
-            //as = new AudioStream (new FileInputStream ("music.wav"));
-            //AudioPlayer.player.start (as);
-        }
-        catch (Exception e)
-        {
-        }
     }
 
 
@@ -35,33 +34,32 @@ public class Scoreboard
 
     public void draw ()
     {
-        char pressed = c.getChar ();
-        c.setColor (Colours.white);
+        c.getChar (); // gets the user to enter a key before returning to the menu
+        c.setColor (Colours.white); // background
         c.fillRect (0, 0, 640, 500);
-        c.setColor (Colours.lightBlue);
+        c.setColor (Colours.lightBlue); 
         c.fillRect (135, 100, 355, 330);
         c.setColor (Colours.black);
         c.setFont (new Font ("Ariel", Font.BOLD, 45));
-        c.drawString ("High Scores", 135, 80);
+        c.drawString ("High Scores", 135, 80); // title
         
         c.setFont (new Font ("Ariel", Font.PLAIN, 35));
-        try
+        try // reads from the file and displays it on the screen
         {
             BufferedReader in;
             String line;
             int count = 0;
             boolean foundName = false;
-            in = new BufferedReader (new FileReader ("scoreboard.txt"));
-            while (true)
+            in = new BufferedReader (new FileReader ("scoreboard.txt")); // reads from scoreboard.txt
+            while (true) // loops through each line
             {
                 count++;
-                // if the store name is found:
                 line = in.readLine ();
-                if (count > 8)
+                if (count > 8) // only displays the top few scores (so it looks nicer)
                     break;
-                if (line == null)
+                if (line == null) // breaks if it reads an empty line (no more scores)
                     break;
-                c.drawString (line, 140, 120+count*35);
+                c.drawString (line, 140, 120+count*35); // displays the line of the file
             }
             in.close ();
         }
@@ -70,6 +68,6 @@ public class Scoreboard
         }
 
         c.setFont (new Font ("Ariel", Font.BOLD, 25));
-        c.drawString ("Press any key to return", 140, 450);
+        c.drawString ("Press any key to return", 140, 450); // prompts the user to enter a key to return
     }
 }
